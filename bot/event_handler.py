1c4e52911743628b062cebae7767631789266c04
davenewham <davidpaulnewham@gmail.com>
2016-05-22 16:01:03 +0100

    Update event_handler.py

--- bot/event_handler.py
+++ bot/event_handler.py
@@ -46,6 +46,8 @@ class RtmEventHandler(object):
                     self.msg_writer.write_greeting(event['channel'], event['user'])
                 elif 'joke' in msg_txt:
                     self.msg_writer.write_joke(event['channel'])
+                elif 'kill yourself' in msg_text:
+                    self.msg+writer.write_killyourself(event['channel'])
                 elif 'attachment' in msg_txt:
                     self.msg_writer.demo_attachment(event['channel'])
                 else:
