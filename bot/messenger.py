9133ad7f8ae42b11272214ca1b4ed13341fa006c
davenewham <davidpaulnewham@gmail.com>
2016-05-22 22:10:03 +0100

    Update messenger.py

--- bot/messenger.py
+++ bot/messenger.py
@@ -24,28 +24,22 @@ class Messenger(object):
             "> `<@" + bot_uid + "> joke` - I'll tell you one of my finest jokes, with a typing pause for effect. :laughing:",
             "> `<@" + bot_uid + "> attachment` - I'll demo a post with an attachment using the Web API. :paperclip:")
         self.send_message(channel_id, txt)
-    
-    def killyourself(self, channel_id, user_id):
-        main = ['I will gladly kill myself']
-        txt = '{}, <@{}>!'.format(random.choice(greetings), user_id)
-        self.send_message(channel_id, txt)
-    
+
     def write_greeting(self, channel_id, user_id):
         greetings = ['You are a dirty whore']
         txt = '{}, <@{}>!'.format(random.choice(greetings), user_id)
         self.send_message(channel_id, txt)
 
-    def write_prompt(self, channel_id, user_id):
+    def write_prompt(self, channel_id):
         bot_uid = self.clients.bot_user_id()
-        txtsub  = ["I'm sorry, I didn't quite understand... Please kill yourself"]
-        txt = '{}, <@{}>!'.format(random.choice(txtsub), user_id)
+        txt = "I'm sorry, I didn't quite understand... Can I help you? (e.g. `<@" + bot_uid + "> help`)"
         self.send_message(channel_id, txt)
 
     def write_joke(self, channel_id):
-        question = "Why did the slav cross the road?"
+        question = "Why did the python cross the road?"
         self.send_message(channel_id, question)
         self.clients.send_user_typing_pause(channel_id)
-        answer = "I don't know :laughing:"
+        answer = "To eat the chicken on the other side! :laughing:"
         self.send_message(channel_id, answer)
 
 
@@ -54,14 +48,14 @@ class Messenger(object):
         self.send_message(channel_id, txt)
 
     def demo_attachment(self, channel_id):
-        txt = "spaghetti"
+        txt = "Beep Beep Boop is a ridiculously simple hosting platform for your Slackbots."
         attachment = {
-            "pretext": "We give life to slavs :sunglasses: :thumbsup:",
-            "title": "when ur spag falls out ur pockets",
-            "title_link": "8ch.net/hebe",
+            "pretext": "We bring bots to life. :sunglasses: :thumbsup:",
+            "title": "Host, deploy and share your bot in seconds.",
+            "title_link": "https://beepboophq.com/",
             "text": txt,
             "fallback": txt,
-            "image_url": "http://i.imgur.com/jtmyB.png",
+            "image_url": "https://storage.googleapis.com/beepboophq/_assets/bot-1.22f6fb.png",
             "color": "#7CD197",
         }
         self.clients.web.chat.post_message(channel_id, txt, attachments=[attachment], as_user='true')
