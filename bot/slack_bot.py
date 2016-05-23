f2a34efe810f631f3ebbfea73199b13a952902b7
davenewham <davidpaulnewham@gmail.com>
2016-05-23 22:35:34 +0100

    Update slack_bot.py

--- bot/slack_bot.py
+++ bot/slack_bot.py
@@ -25,6 +25,44 @@ class SlackBot(object):
         if token is not None:
             self.clients = SlackClients(token)
 
+    def start(self, resource):
+        """Creates Slack Web and RTM clients for the given Resource
+        using the provided API tokens and configuration, then connects websocket
+        and listens for RTM events.
+
+        Args:
+            resource (dict of Resource JSON): See message payloads - https://beepboophq.com/docs/article/resourcer-api
+        """
+        logger.debug('Starting bot for resource: {}'.format(resource))
+        if 'resource' in resource and 'SlackBotAccessToken' in resource['resource']:
+            res_access_token = resource['resource']['SlackBotAccessToken']
+            self.clients = SlackClients(res_access_token)
+
+        if self.clients.rtm.rtm_connect():
+            logging.info(u'Connected {} to {} team at https://{}.slack.com'.format(
+                self.clients.rtm.server.username,
+                self.clients.rtm.server.login_data['team']['name'],
+                self.clients.rtm.server.domain))
+
+            msg_writer = Messenger(self.clients)
+            event_handler = RtmEventHandler(self.clients, msg_writer)
+
+            while self.keep_running:
+                for event in self.clients.rtm.rtm_read():
+                    try:
+                        event_handler.handle(event)
+                    except:
+                        err_msg = traceback.format_exc()
+                        logging.error('Unexpected error: {}'.format(err_msg))
+                        msg_writer.write_error(event['channel'], err_msg)
+                        continue
+
+                self._auto_ping()
+                time.sleep(.1)
+
+        else:
+            logger.error('Failed to connect to RTM client with token: {}'.format(self.clients.token))
+
     def _auto_ping(self):
         # hard code the interval to 3 seconds
         now = int(time.time())
