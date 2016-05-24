977075594d02d98be715d7948101a43a6b78a52f
radicaldude <joe.shutts@gmx.co.uk>
2016-05-24 21:45:08 +0100

    Update __init__.py

--- bot/__init__.py
+++ bot/__init__.py
@@ -3,16 +3,16 @@ fp = open('./resources/Jokes.txt', 'r')
 tmp_str=fp.readline()
 global joke_list 
 joke_list = []
-while tmp_str and tmp_str!="\n" and tmp_str!= "\r":
-  tmp_str = fp.readline()
+while tmp_str and tmp_str=="":
+  tmp_str = fp.readline().strip()
 while tmp_str:
   tmp_item=[]
-  if tmp_str!="\n" and tmp_str!= "\r":
+  if tmp_str != "":
     tmp_item.append(tmp_str)
    else:
      break
-   tmp_str = fp.readline()
-   if tmp_str!='\n' and tmp_str!= "\r":
+   tmp_str = fp.readline().stri()
+   if tmp_str!= ""
      tmp_item.append(tmp_str)
    joke_list.append(tmp_item)
    tmp_str = fp.readline()
