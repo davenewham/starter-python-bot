6a5ff83923b9e2c7de4063dd9e8b2587948264fe
radicaldude <joe.shutts@gmx.co.uk>
2016-05-24 21:05:24 +0100

    Update __init__.py

--- bot/__init__.py
+++ bot/__init__.py
@@ -1,10 +1,10 @@
 import os.path
 fp = open(os.path.dirname(__file__) + '/../resources/jokes.txt', 'r')
 tmp_str=fp.readline()
+global joke_list = []
 while tmp_str and tmp_str!='\n' and tmp_str!= '\r':
   tmp_str = fp.readline()
 if(tmp_str):
-  joke_list=[]
   while tmp_str:
     tmp_item=[]
     if tmp_str!='\n' and tmp_str!= '\r':
