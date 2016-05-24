4b54e63342128bcf39ca66e87f8a0ace7f4fd22f
radicaldude <joe.shutts@gmx.co.uk>
2016-05-24 21:16:19 +0100

    Update __init__.py

--- bot/__init__.py
+++ bot/__init__.py
@@ -1,5 +1,5 @@
 import os.path
-fp = open(os.path.dirname(__file__) + '/../resources/jokes.txt', 'r')
+fp = open('./resources/jokes.txt', 'r')
 tmp_str=fp.readline()
 global joke_list 
 joke_list = []
