91d0cc05d66014229384ea7b329ff1e8fa2b30b1
radicaldude <joe.shutts@gmx.co.uk>
2016-05-24 22:02:18 +0100

    Update __init__.py

--- bot/__init__.py
+++ bot/__init__.py
@@ -1,19 +1,18 @@
-import os.path
 fp = open('./resources/Jokes.txt', 'r')
 tmp_str=fp.readline().strip()
-global joke_list 
+global joke_list
 joke_list = []
 while tmp_str and tmp_str=="":
-  tmp_str = fp.readline().strip()
+tmp_str = fp.readline().strip()
 while tmp_str:
-  tmp_item=[]
-  if tmp_str != "":
-    tmp_item.append(tmp_str)
-  else:
-    break
-  tmp_str = fp.readline().strip()
-  if tmp_str!= "":
-    tmp_item.append(tmp_str)
-  joke_list.append(tmp_item)
-  fp.readline()
-  tmp_str = fp.readline().strip()
+tmp_item=[]
+if tmp_str != "":
+tmp_item.append(tmp_str)
+else:
+break
+tmp_str = fp.readline().strip()
+if tmp_str!= "":
+tmp_item.append(tmp_str)
+joke_list.append(tmp_item)
+fp.readline()
+tmp_str = fp.readline().strip()
