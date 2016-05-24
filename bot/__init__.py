0906f6c0583bc20c320de2b9916f540142ca51bc
radicaldude <joe.shutts@gmx.co.uk>
2016-05-24 18:42:33 +0100

    Update __init__.py
    Sets up joke array

---
+++ bot/__init__.py
@@ -0,0 +1,18 @@
+import os.path
+fp = open(os.path.dirname(__file__) + '/../resources/jokes.txt', 'r')
+tmp_str=fp.readline()
+while tmp_str and tmp_str!='\n' and tmp_str!= '\r':
+  tmp_str = fp.readline()
+if(tmp_str):
+  joke_list=[]
+  while tmp_str:
+    tmp_item=[]
+    if tmp_str!='\n' and tmp_str!= '\r':
+      tmp_item.append(tmp_str)
+    else:
+      break
+    tmp_str = fp.readline()
+    if tmp_str!='\n' and tmp_str!= '\r':
+      tm_item.append(tmp_str)
+    joke_list.append(tmp_item)
+    tmp_str = fp.readline()
