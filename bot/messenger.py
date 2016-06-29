5885c4367428c4e3e25bec327d1edf8fac6fd545
Harvey <Harvey@Harveys-iMac.local>
2016-06-29 21:14:17 +0100

    www
    w

--- bot/messenger.py
+++ bot/messenger.py
@@ -109,12 +109,13 @@ class Messenger(object):
         #Do not overuse this - Limited to 2000 requests/per month
         apikey = "77JTTE-3JLXVRWY9P"
 
-        query = urllib.quote_plus(query)
+        self.send_message(channel_id, query)
         print("Attempting to query '" + query + "'")
         url = "http://api.wolframalpha.com/v2/query?input=" + query + "&appid=" + apikey
         httpsocket = urllib.urlopen(url)
         httpbody = httpsocket.read()
         httpsocket.close()
+        self.send_message(channel_id, len(httpbody))
 
         parser = xml.sax.make_parser()
         parser.setFeature(xml.sax.handler.feature_namespaces, 0)
