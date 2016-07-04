f53060ae86ebda679e86eced488d4b30cdf00dfc
Tyler Farghly <tylerf98@live.co.uk>
2016-07-04 23:26:39 +0100

    x

--- bot/event_handler.py
+++ bot/event_handler.py
@@ -12,8 +12,8 @@ class RtmEventHandler(object):
     def __init__(self, slack_clients, msg_writer):
         self.clients = slack_clients
         self.msg_writer = msg_writer
-        
-        self.msg_writer.startWhy(TILERID);
+
+        #self.msg_writer.startWhy(TILERID);
 
     def handle(self, event):
 
